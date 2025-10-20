e44c8ea9723287ddb2dee472728714a8838f0d1e
Ariel Mercado <arielmercadoburgos@gmail.com>
2025-10-20 08:32:40 -0300

    Version inicial del proyecto dgpejbf-portal

--- webpack/proxy.conf.js
+++ webpack/proxy.conf.js
@@ -3,7 +3,7 @@ function setupProxy({ tls }) {
   return [
     {
       context: serverResources,
-      target: `http${tls ? 's' : ''}://localhost:8080`,
+      target: `http${tls ? 's' : ''}://localhost:9191`,
       secure: false,
       changeOrigin: tls,
     },
